5298c1a7ab5b7d437894a79d13c8433cb0502786
w2xi <43wangxi@gmail.com>
2022-06-07 18:37:30 +0800

    chore: 创建继承自Array.prototype的中间对象并重写该对象 让这个中间对象成为待观测数组的原型

--- src/observer.js
+++ src/observer.js
@@ -1,18 +1,66 @@
-export default class Observer {
+import Dep from './dep.js';
+
+// 劫持重写数组的原型方法，而为了污染污染 Array.prototype，可以对需要观测的数组进行特殊处理
+// 通过 Object.create(Array.prototype) 创建一个中间对象(委托自 Array.prototype)，
+// 在这个中间对象上重写 push、pop、shift、unshift、splice、sort、reverse 等方法，
+// 就不会污染 Array.prototype，然后让这个中间对象成为 待观测数组的原型( targetArray.__proto__ = arrayMethods)
+
+const arrayProto = Array.prototype;
+const arrayMethods = Object.create(arrayProto);
+const methodsToPatch = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse'
+];
+
+methodsToPatch.forEach(method => {
+  // 缓存原型自身的方法
+  const original = arrayProto[method];
+
+  Object.defineProperty(arrayMethods, method, {
+    value: function mutator(...args) {
+      // 调用原型方法
+      const result = original.apply(this, args);
+      return result;
+    },
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  });
+})
+
+class Observer {
   constructor(data) {
     this.data = data;
 
-    if (!Array.isArray(data)) {
+    if (Array.isArray(data)) {
+      this.observeArray(data);
+    } else {
       this.walk(data);
     }
   }
 
   walk(data) {
     for (let key in data) {
-      if (Reflect.has(data, key)) {
+      if (hasOwnProperty.call(data, key)) {
         defineReactive(data, key, data[key]);
       }
     }
+  }
+
+  observeArray(data) {
+    data.__proto__ = arrayMethods;
+    data.forEach(item => {
+      if (item && typeof item === 'object') {
+        this.walk(item);
+      } else {
+        defineReactive(data, item, data[item]);
+      }
+    });
   }
 }
 
@@ -37,3 +85,5 @@ export function defineReactive(data, key, value) {
     },
   });
 }
+
+export default Observer;
