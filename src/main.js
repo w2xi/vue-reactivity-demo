6985b33c704027037bf56612279ca786c4db2e6a
w2xi <43wangxi@gmail.com>
2022-07-25 18:16:29 +0800

    chore: update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from './index.js';
+import MiniVue from './index.js';
 
-const vm = new Vue({
+const vm = new MiniVue({
   data: {
     message: 'Hello Vue!',
     obj: {
       name: 'asdf',
       age: 12,
     }
+  },
+  methods: {
+    onClick() {
+      debugger;
+      console.log('clicked');
+      this.$set(this.obj, 'address', 'China');
+    },
   }
 }).$mount('#app');
 
@@ -17,9 +24,11 @@ const vm = new Vue({
 // // 取消观察数据
 // unwatch();
 
-vm.$watch('obj', (newVal, oldVal) => {
-  console.log('[new value]', newVal);
-  console.log('[old value]', oldVal);
-}, { deep: true, immediate: false });
+// todo#1 bug
+// deep: true 参数无法深度监听
+// vm.$watch('obj', (newVal, oldVal) => {
+//   console.log('[new value]', newVal);
+//   console.log('[old value]', oldVal);
+// }, { deep: true, immediate: true });
 
 window.vm = vm;
